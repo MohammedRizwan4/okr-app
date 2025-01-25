a25c31880a3be0d0c05bc21e9b6dd1d6f7bb2a5f
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2025-01-25 15:00:44 +0530

    refactor: removed old design and will rebuild with modern UI

---
+++ src/TailwindDemo.tsx
@@ -0,0 +1,29 @@
+const TailwindDemo = () => {
+    return <div className="">
+        <div className="">
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+        </div>
+    </div>;
+};
+
+export default TailwindDemo;
