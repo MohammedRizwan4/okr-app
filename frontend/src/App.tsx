fcb59259250ad6af97332d3c6ce00ad02147d66b
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2025-02-10 08:21:03 +0530

    remove Auth SignIn with firebase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,8 @@ import Navbar from "./components/Navbar.tsx";
 import {Navigate, Route, Routes} from "react-router";
 import CreateOKRForm from "./components/CreateOKRForm.tsx";
 import DisplayOKRs from "./components/DisplayOKRs.tsx";
-import {onAuthStateChanged} from "firebase/auth";
 import {useEffect, useState} from "react";
 import {ObjectiveType} from "./types/OKRtypes.ts";
-import {auth} from "./firebase/firbaseConfig.ts";
-import Login from "./components/Login.tsx";
-import {Atom} from "react-loading-indicators";
-import { User } from "firebase/auth";
 
 const App = () => {
 
@@ -17,8 +12,6 @@ const App = () => {
     });
 
     const [objectives, setObjectives] = useState<ObjectiveType[]>([]);
-    const [user, setUser] = useState<User | null>(null);
-    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
 
@@ -31,35 +24,17 @@ const App = () => {
             setIsDarkMode(false)
         }
 
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-            // setTimeout(() => {
-            setIsLoading(false)
-            // }, 3000)
-        });
-
-        return () => unsubscribe();
     }, []);
 
-    if (isLoading) {
-        return <div className="h-screen flex items-center justify-center text-lg">
-            <Atom color="rgb(14 165 233 / var(1, 1))" size="medium" text="" textColor="rgb(14 165 233 / var(1, 1))"/>
-        </div>;
-    }
 
     return <div className="h-screen flex flex-col text-gray-700">
-        <Navbar user={user} isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+        <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
         <div className="flex-grow">
-            {
-                user && <Navbar user={user} isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
-            }
             <Routes>
-                <Route path="/login" element={user ? <Navigate to="/view-okrs"/> : <Login/>}/>
-                <Route path="/create-okr" element={user ? <CreateOKRForm/> : <Navigate to="/login"/>}/>
+                <Route path="/create-okr" element={<CreateOKRForm/>}/>
                 <Route path="/view-okrs"
-                       element={user ? <DisplayOKRs objectives={objectives} setObjectives={setObjectives}/> :
-                           <Navigate to="/login"/>}/>
-                <Route path="*" element={<Navigate to={user ? "/view-okrs" : "/login"}/>}/>
+                       element={<DisplayOKRs objectives={objectives} setObjectives={setObjectives}/>}/>
+                <Route path="*" element={<Navigate to="/view-okrs"/>}/>
             </Routes>
         </div>
     </div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,11 +2,8 @@ import {NavLink} from "react-router";
 import {FaMoon} from "react-icons/fa6";
 import React from "react";
 import {MdLightMode} from "react-icons/md";
-import {signOut, User} from "firebase/auth";
-import {auth} from "../firebase/firbaseConfig.ts";
 
-const Navbar = ({user, isDarkMode, setIsDarkMode}: {
-    user: User | null,
+const Navbar = ({ isDarkMode, setIsDarkMode}: {
     isDarkMode: boolean,
     setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>
 }) => {
@@ -23,38 +20,19 @@ const Navbar = ({user, isDarkMode, setIsDarkMode}: {
         }
     };
 
-    const handleLogout = async () => {
-        await signOut(auth);
-    };
-
     return <div
         className="fixed  z-40  bg-white dark:bg-dark dark:border-b dark:border-gray-800 w-full h-16 flex justify-between items-center  dark:text-white ">
         <div className="ml-5 cursor-pointer">
             <img src="../../public/vite.svg" alt=""/>
         </div>
         <div className="flex gap-16 mr-10  font-medium flex-nowrap items-center justify-end text-lg">
-            {
-                user ? (<>
-                    <NavLink to="/create-okr" className="">
-                        Create OKR
-                    </NavLink>
-                    <NavLink to="/view-okrs" className="">
-                        Your OKR
-                    </NavLink>
-                    <button
-                        onClick={handleLogout}
-                        className="bg-red-500 text-white px-4 py-1 rounded-lg hover:bg-red-700"
-                    >
-                        Logout
-                    </button>
-                </>) : (<NavLink
-                    to="/login"
-                    className="bg-blue-500 text-white px-4 py-1 rounded-lg hover:bg-blue-700"
-                >
-                    Login
-                </NavLink>)
-            }
 
+            <NavLink to="/create-okr" className="">
+                Create OKR
+            </NavLink>
+            <NavLink to="/view-okrs" className="">
+                Your OKR
+            </NavLink>
             {
                 isDarkMode ? <FaMoon onClick={toggleDarkMode}
                                      className="text-4xl cursor-pointer p-2 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-full"/> :
